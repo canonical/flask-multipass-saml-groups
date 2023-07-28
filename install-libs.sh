712f905ac52630277598133e620f42c67d3a89a6
Christopher Bartz <christopher.bartz@canonical.com>
2023-07-28 14:20:24 +0200

    Adapt README

--- install-libs.sh
+++ install-libs.sh
@@ -2,5 +2,8 @@
 #  Copyright 2023 Canonical Ltd.
 #  See LICENSE file for licensing details.
 
+# Script that installs the required native libraries for installing the plugin's dependencies
+# on Ubuntu 22.04.
+
 sudo apt update
 sudo apt install -y pkg-config libxml2-dev libxmlsec1-dev libxmlsec1-openssl libpython3-dev gcc libpq-dev
