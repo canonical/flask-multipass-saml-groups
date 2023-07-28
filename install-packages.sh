f201d224be12d2f32afd8e48df2a3161f3bab0e7
Christopher Bartz <christopher.bartz@canonical.com>
2023-07-28 13:47:48 +0200

    Add pre-run-script to install xmlsec deps

---
+++ install-packages.sh
@@ -0,0 +1,5 @@
+#  Copyright 2023 Canonical Ltd.
+#  See LICENSE file for licensing details.
+
+sudo apt update
+sudo apt install -y pkg-config libxml2-dev libxmlsec1-dev libxmlsec1-openssl libpython3-dev gcc
