a477a87c5940a8d150bdbf7e18ca1812d85ac629
Christopher Bartz <christopher.bartz@canonical.com>
2023-07-28 13:57:29 +0200

    Add libpq-dev to install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 #  Copyright 2023 Canonical Ltd.
 #  See LICENSE file for licensing details.
 
 sudo apt update
-sudo apt install -y pkg-config libxml2-dev libxmlsec1-dev libxmlsec1-openssl libpython3-dev gcc
+sudo apt install -y pkg-config libxml2-dev libxmlsec1-dev libxmlsec1-openssl libpython3-dev gcc libpq-dev
