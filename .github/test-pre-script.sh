554ab74a63b1f519fd0afb4c92dc96a9fd61f56e
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2024-04-05 14:12:42 +0200

    python3.12 with pyenv

--- .github/test-pre-script.sh
+++ .github/test-pre-script.sh
@@ -4,3 +4,14 @@
 
 # Call script with sudo to install the required native libraries for installing the plugin's dependencies
 sudo bash -xe "$(dirname "$0")"/../install-libs.sh
+
+# Install python 3.12.2
+# We don't activate after installing since this is not required by default
+# python 3.12.3 will be installed in $HOME/.pyenv/versions/3.12.2/bin
+curl https://pyenv.run | bash
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv install 3.12.2
+pyenv doctor
